427b2e7004ed3406a6340e88504742e1cc801328
Tereami <undress.my.future@gmail.com>
2023-04-07 21:47:57 +0300

    add user and role

---
+++ DomainModel/Identity/Role.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace DomainModel.Identity
+{
+    public class Role : IdentityRole
+    {
+        public const string Administrators = "Administrators";
+        public const string Users = "Users";
+    }
+}
---
+++ DomainModel/Identity/User.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DomainModel.Identity
+{
+    public class User : IdentityUser
+    {
+        public const string Administrator = "Admin";
+        public const string DefaultAdminPassword = "qwerty";
+    }
+}
